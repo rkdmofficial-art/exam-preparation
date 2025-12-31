4afacd334cb9721099c5a7b56a0920188cb40756
rkdmofficial-art <rkdmofficial@gmail.com>
2025-12-31 08:49:24 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -68,7 +68,7 @@ async function authAction(actionType) {
             if (actionType === 'login') {
                 localStorage.setItem('isLoggedIn', 'true');
                 localStorage.setItem('userName', result.firstName);
-                window.location.href = 'Main.html';
+                window.location.href = 'pages/dashboard.html';
             } else if (actionType === 'sendOTP') {
                 document.getElementById('step1').style.display = 'none';
                 document.getElementById('otpSection').style.display = 'block';
